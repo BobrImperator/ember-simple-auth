46689fad9e768836805bc59c6b759aac6cc46315
Bartlomiej Dudzik <bartholomew.dudzik@gmail.com>
2023-04-21 22:17:05 +0200

    --wip-- [skip ci]

--- packages/test-app/fastboot-server.js
+++ packages/test-app/fastboot-server.js
@@ -1,11 +1,12 @@
 'use strict';
 
 const FastBootAppServer = require('fastboot-app-server');
+const config = require('./config/fastboot')(process.env.NODE_ENV);
 
-let server = new FastBootAppServer({
+let server = new FastBootAppServer(Object.assign({
   distPath: 'dist',
   port: process.env.PORT || 3000,
-  gzip: true // Optional - Enables gzip compression.
-});
+  gzip: true, // Optional - Enables gzip compression.
+}, config));
 
 server.start();
